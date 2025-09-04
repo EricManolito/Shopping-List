8ec07419399b8e49e70457aa3cf555a7058665ef
EricManolito <emanolito@cnm.edu>
2025-09-04 15:41:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    initShoppingList();
+};
+/*console.log("Test");*/
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+    console.log(form)
+}
